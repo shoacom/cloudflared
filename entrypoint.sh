51c5b7f94d14f771c2376a4481bddf1dff727926
shoacom <837907540@qq.com>
2023-05-06 06:08:13 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,4 @@
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb &&
 dpkg -i cloudflared.deb &&
 cloudflared service install eyJhIjoiZTcwYzcxNzU1MzUzMDFiMTAxYWY2ZDA2NTU2ZGNkZGQiLCJ0IjoiNjA2Y2I5N2MtZmY5Mi00OTQ1LWE2ZWQtZTFlNWRhNzhhY2I4IiwicyI6Ik1EbGlZbVpsTnpFdFpXWTJZeTAwWkdNd0xUZ3dZamN0TlRabE5UWmhORFU1TldOaSJ9
+tail -f /dev/null
